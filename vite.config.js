55a5bb97e6b222887e32b8a37df3f50f6ff5d93e
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-14 12:26:45 +0100

    Further project setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import { patchCssModules } from 'vite-css-modules';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), patchCssModules()],
   test: {
     globals: true,
     environment: 'jsdom',
