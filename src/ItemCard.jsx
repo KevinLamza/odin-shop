bed7edabea52e8288af4198e9698e7922b9bff7c
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2024-12-14 15:37:23 +0100

    Add pixelperfect image scalingn

--- src/ItemCard.jsx
+++ src/ItemCard.jsx
@@ -2,10 +2,22 @@ import styles from './ItemCard.module.css';
 import ultraball from './assets/ultra_ball.webp';
 
 const ItemCard = (item) => {
+    // Get canvas context
+    const ctx = document.getElementById('game').getContext('2d');
+
+    // Load image
+    const image = new Image();
+    image.onload = () => {
+        // Draw the image into the canvas
+        ctx.drawImage(image, 0, 0);
+    };
+    image.src = ultraball;
     return (
         <>
             <div className={styles.itemContainer}>
-                <img src={ultraball}></img>
+                <canvas id="game" width="24" height="24">
+                    A cat
+                </canvas>
                 <div>
                     <h2>TITLE</h2>
                     <p>DESCRIPTION</p>
